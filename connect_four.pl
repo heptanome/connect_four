b80390784843db511387b8137d6d536063e48190
Guillaume <guillaume@kessibi.fr>
2020-11-26 09:13:06 +0100

    project: printing is now in a different, helper file

--- connect_four.pl
+++ connect_four.pl
@@ -1,5 +1,7 @@
 :- dynamic board/1.
 
+:- use_module(displayBoard, [displayBoard/1]).
+
 %Regarder si le jeu est fini
 gameover(Winner) :- board(Board), winner(Board, Winner), !.
 gameover('Draw') :- board(Board), isBoardFull(Board).
@@ -58,10 +60,8 @@ applyIt(Board,NewBoard) :- retract(board(Board)), assert(board(NewBoard)).
 changePlayer('1','2').
 changePlayer('2','1').
 
-
 %IA ernvoie colonne complete
 ia(Board, IndexColonne,_) :- repeat, I is random(7), IndexColonne is I+1,  nth1(IndexColonne, Board, Elem), not(isColonneFull(Elem)), write('Chose column '),writeln(IndexColonne), !.
-
 
 
 % Game is over, we cut to stop the search, and display the winner.
@@ -72,51 +72,17 @@ play(Player) :- write('New turn for:'), writeln(Player), board(Board), displayBo
 
 % Joue le coup (pour l'instant par du principe que la colonne donnee
 % n'est pas pleine
-
-
 playMove(Board,IndexColonne,NewBoard,P) :- Board=NewBoard,
     nth1(IndexColonne, Board, Colonne),
     updateColonne(Colonne, NewColonne, P),
     nth1(IndexColonne, NewBoard, NewColonne).
 
-
-
 updateColonne(Colonne,NewColonne, Player) :- Colonne = NewColonne,
     nth1(Index,Colonne,X), var(X),!,
     nth1(Index, NewColonne, Player).
-
-
-
-
-
-
-%Affichage du jeu : ligne par ligne
-displayBoard(B) :- writeln('*------------*'),
-    printLigne(6,B), printLigne(5,B), printLigne(4,B),
-    printLigne(3,B), printLigne(2,B), printLigne(1,B),
-    writeln('*------------*').
-
-%Affichage d'une ligne
-printLigne(L, B) :- nth1(1,B,C1), printVal(C1,L),
-    nth1(2,B,C2), printVal(C2,L),
-    nth1(3,B,C3), printVal(C3,L),
-    nth1(4,B,C4), printVal(C4,L),
-    nth1(5,B,C5), printVal(C5,L),
-    nth1(6,B,C6), printVal(C6,L),
-    nth1(7,B,C7), printVal(C7,L),
-    writeln('').
-
-% Afficher le contenu de la case a l'indice N de la colonne C (?, x or
-% o)
-printVal(C,N) :- nth1(N,C,Val), var(Val), write('  '), ! .
-printVal(C,N) :- nth1(N,C,Val), write(Val), write(' ').
-
 
 %Debut du jeu
 init :- length(C1,6), length(C2,6),length(C3,6),
     length(C4,6),length(C5,6),length(C6,6),length(C7,6),
     assert(board([C1,C2,C3,C4,C5,C6,C7])), play('1').
-
-
-
 
---
+++ displayBoard.pl
@@ -0,0 +1,22 @@
+:- module(displayBoard, [displayBoard/1]).
+
+%Affichage du jeu : ligne par ligne
+displayBoard(B) :- writeln('*------------*'),
+    printLigne(6,B), printLigne(5,B), printLigne(4,B),
+    printLigne(3,B), printLigne(2,B), printLigne(1,B),
+    writeln('*------------*').
+
+%Affichage d'une ligne
+printLigne(L, B) :- nth1(1,B,C1), printVal(C1,L),
+    nth1(2,B,C2), printVal(C2,L),
+    nth1(3,B,C3), printVal(C3,L),
+    nth1(4,B,C4), printVal(C4,L),
+    nth1(5,B,C5), printVal(C5,L),
+    nth1(6,B,C6), printVal(C6,L),
+    nth1(7,B,C7), printVal(C7,L),
+    writeln('').
+
+% Afficher le contenu de la case a l'indice N de la colonne C (?, x or o)
+printVal(C,N) :- nth1(N,C,Val), var(Val), write('  '), ! .
+printVal(C,N) :- nth1(N,C,Val), write(Val), write(' ').
+
