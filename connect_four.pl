884f0bca87289a38ea1247e815ffae6bf92635a4
Guillaume <guillaume@kessibi.fr>
2020-11-27 11:44:12 +0100

    connet_four: can now select two different heuristics

--- connect_four.pl
+++ connect_four.pl
@@ -23,12 +23,49 @@ changePlayer('2','1').
 % ia(Board, IndexColonne,_) :- repeat, I is random(7), IndexColonne is I+1,  nth1(IndexColonne, Board, Elem), not(isColonneFull(Elem)), write('Chose column '),writeln(IndexColonne), !.
 ia(Board, BestNext, Value, Player) :- find_best_next_pos(Board, BestNext, Value, Player).
 
+readColonne(X) :-
+    write('Quelle colonne voulez-vous jouer (1 à 7)?'),
+    read(X),
+    nth1(Index, [1,2,3,4,5,6,7], X),
+    not(isColonneFull(Index)).
 
 % Game is over, we cut to stop the search, and display the winner.
-play(_) :- gameover(Winner), !, write('Game is Over. Winner: '), writeln(Winner), board(Board),displayBoard(Board).
+play(_,_,_) :- gameover(Winner), !, write('Game is Over. Winner: '), writeln(Winner), board(Board),displayBoard(Board).
 % The game is not over, we play the next turn
-%play(Player) :- write('New turn for:'), writeln(Player), board(Board), displayBoard(Board),ia(Board, IndexColonne, Player), playMove(Board, IndexColonne, NewBoard, Player),  applyIt(Board, NewBoard), changePlayer(Player,NextPlayer), play(NextPlayer).
-play(Player) :- write('New turn for:'), writeln(Player), board(Board), displayBoard(Board),ia(Board, NewBoard, _, Player),  applyIt(Board, NewBoard), changePlayer(Player,NextPlayer), play(NextPlayer).
+
+% in case the player 1 is human
+% Heur 1: heuristic used by AI 1, same goes for Heur2
+play(Player, 'human', Heur2) :-
+    Player = '1',
+    write('New turn for:'),
+    writeln(Player), board(Board),
+    displayBoard(Board),
+    readColonne(IndexColonne),
+    playMove(Board, IndexColonne, NewBoard, Player),
+    applyIt(Board, NewBoard),
+    changePlayer(Player,NextPlayer),
+    play(NextPlayer, 'human', Heur2), !.
+
+play(Player, Heur1, Heur2) :-
+    Player = '1',
+    write('New turn for:'),
+    writeln(Player), board(Board),
+    displayBoard(Board),
+    ia(Board, NewBoard, _, Player),
+    applyIt(Board, NewBoard),
+    changePlayer(Player,NextPlayer),
+    play(NextPlayer, Heur1, Heur2), !.
+
+play(Player, Heur1, Heur2) :-
+    Player = '2',
+    write('New turn for:'),
+    writeln(Player),
+    board(Board),
+    displayBoard(Board),
+    ia(Board, NewBoard, _, Player),
+    applyIt(Board, NewBoard),
+    changePlayer(Player,NextPlayer),
+    play(NextPlayer, Heur1, Heur2).
 
 % Joue le coup (pour l'instant par du principe que la colonne donnee
 % n'est pas pleine
@@ -52,5 +89,5 @@ init :-
     assert(board([C1,C2,C3,C4,C5,C6,C7])).
 
 % lancer le jeu
-start_game :- init(), play('1').
+start_game(Heur1, Heur2) :- init(), play('1', Heur1, Heur2).
 
