92c7949b90161c19119ef8a0786411f6ee40d630
Kmilia <camelia.guerraoui@gmail.com>
2020-11-27 12:11:47 +0100

    L'heuristique prend en compte les lignes et les diagonales

--- firstHeuristic.pl
+++ firstHeuristic.pl
@@ -5,39 +5,61 @@
 % - Board : état du plateau après avoir joué le coup
 % - Player : numéro du joueur 1 ou 2
 % - Cout : cout du board
-heuristic(Board, Player, Cout) :-
-    getListCoutColonne(Board, Player, ListCouts),
-    max_list(ListCouts, CoutMaxColonne),
-    Cout is 4-CoutMaxColonne.
+heuristic(Board, Player, FinalCost) :-
+    getColumnCostList(Board, Player, CostsListColumn),
+    getRowCostList(Board, Player, CostsListRow),
+    append(CostsListColumn, CostsListRow, CostsListAll),
+    max_list(CostsListAll, MaxCost),
+    FinalCost is 4-MaxCost.
 
-%Cette methode compte le nombre de pions alignés d un joueur sur une colonne en partant de la fin de la colonne.
-%compteAligne(+Board, +Player, +Move, -CoutColonne) :
+%getColumnCostList(+Board, +Player, -List) :
 % - Board est le plateau de jeu actuel
 % - Player est le numéro du joueur actuel
-% - Move est indice de la colonne sur laquelle on calcule le nombre de jetons alignés
-% - CoutColonne est le nombre de jetons alignés du joueur.   
-getListCoutColonne([], _, []).
-getListCoutColonne([ActualColonne|Rest], Player, [Cout|List]) :-
+% - List est l ensemble des jetons alignés par colonne d un plateau
+getColumnCostList([], _, []).
+getColumnCostList([ActualColonne|Rest], Player, [Cost|List]) :-
 	nonvar(Player),
     reverse(ActualColonne, ReversedColonne),
-    compteColonne(Player, ReversedColonne, Cout),
-    getListCoutColonne(Rest, Player, List).
+    sumColumn(Player, ReversedColonne, Cost),
+    getColumnCostList(Rest, Player, List).
 
-%Cette methode compte le nombre de pions du joueur Player alignés jusqu à trouver un jeton de l autre joueur.
-%compteColonne(+Player, +Colonne, -CoutColonne) :
+%sumColumn(+Player, +Column, -Sum) :
 % - Player est le numéro du joueur actuel
-% - Colonne est la colonne sur laquelle on calcule le nombre de jetons alignés
-% - CoutColonne est le nombre de jetons alignés du joueur.
-compteColonne(_, [], 0).
-compteColonne(Player, [H|T], Compteur) :-
+% - Column est la colonne sur laquelle on calcule le nombre de jetons alignés
+% - AlignedTokenAmount est le nombre de jetons alignés du joueur.
+sumColumn(_, [], 0).
+sumColumn(Player, [H|T], Sum) :-
     var(H), !,
-    compteColonne(Player, T, Compteur).
-compteColonne(Player, [H|_], 0) :-
+    sumColumn(Player, T, Sum).
+sumColumn(Player, [H|_], 0) :-
     H \= Player.
-compteColonne(Player, [H|T], NumberColonne) :-
+sumColumn(Player, [H|T], AlignedTokens) :-
     H = Player,
-    compteColonne(Player, T, Compteur),
-    NumberColonne is Compteur+1.
-    
-testColonneAligne(Player, List) :- getListCoutColonne([[1, 1, 1, _], [1, 2, 1, _], [1, 2, _, _], [2, 1, 1, _]], Player, List).
-testHeuristic(Player, Cout) :- heuristic([[1, 2, 1, _], [1, 2, 2, _], [1, 2, _, _], [2, 2, 2, _]], Player, Cout).
+    sumColumn(Player, T, Sum),
+    AlignedTokens is Sum+1.
+
+getRowCostList([[],[],[],[],[],[],[]],_,[]).
+getRowCostList([[H1|R1], [H2|R2], [H3|R3], [H4|R4], [H5|R5], [H6|R6], [H7|R7]], Player, [MaxCostRow|List]) :-
+    nonvar(Player),
+    CurrentLigne = [H1, H2, H3, H4, H5, H6, H7],
+    sumRow(Player, CurrentLigne, LastSum, ListCost),
+    max_list([LastSum|ListCost], MaxCostRow),
+    getRowCostList([R1,R2,R3,R4,R5,R6,R7], Player,List).
+
+sumRow(_, [], 0, []).
+sumRow(Player, [H|T], Sum, [NewSum|ListSum]) :-
+    (var(H) ; H \= Player),
+    sumRow(Player, T, NewSum, ListSum),
+    Sum is 0.
+sumRow(Player, [H|T], Sum, ListSum) :-
+    nonvar(H),
+    H = Player, 
+    sumRow(Player, T, NewSum, ListSum),
+    Sum is NewSum+1.
+ 
+testSumRow(Player, Sum, ListSum) :- sumRow(Player, [1, 2, 1, _, 1, 2, 1, 2, 1, 1, 2], Sum, ListSum).
+ 
+testGetColumnCostList(Player, List) :- getColumnCostList([[1, 1, 1, _], [1, 2, 1, _], [1, 2, _, _], [2, 1, 1, _]], Player, List).   
+testGetRowCostList(Player, Sum) :- getRowCostList([[1, 1, 1, _], [1, 2, 1, _], [1, 2, _, _], [2, 1, 1, _], [2, 1, 2, _], [2, 2, 2, _], [1, _, _, _]], Player, Sum).
+
+testHeuristic(Player, Cost) :- heuristic([[1, 1, 1, _], [1, 2, 1, _], [1, 2, _, _], [2, 1, 1, _], [2, 1, 2, _], [2, 2, 2, _], [1, _, _, _]], Player, Cost).
