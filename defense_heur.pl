c25f927c92baa0987df5c7334e440d2d441c7a9c
[Camille] <camille.peltier04@gmail.com>
2020-11-29 19:10:57 +0100

    created utilities_heuristics.pl file

--- defense_heur.pl
+++ defense_heur.pl
@@ -1,5 +1,6 @@
 :- module(defense_heur, [heuristic_def/3]).
-
+:- use_module(utilities_heuristics, [getEveryDescDiags/2, reverseEveryColumns/2, 
+    getEveryDescDiagsHalfBoard/3, createOneDescDiag/5]).
 % Usage : Obtenir le coût de la dispotion actuelle du plateau en
 %         cherchant le nombre maximum de jetons alignés du joueur adverse sur le plateau.
 %         Ces jetons peuvent être aligné sur une ligne, une colonne ou une diagonale.
@@ -108,57 +109,6 @@ sumDiag(Player, [Diag|Rest], [MaxCostDiag|ListSum]) :-
     sumRow(Player, Diag, LastSum, ListCost),
     max_list([LastSum|ListCost], MaxCostDiag),
     sumDiag(Player, Rest, ListSum).
-
-% Usage : Créer une liste des diagonales descendantes N°4 à 9.
-% getEveryDescDiags(+Board, -CompleteListDiags)
-% - Board             : état du plateau après avoir joué le coup
-% - CompleteListDiags : liste de toutes les diagonales descendantes du plateau
-getEveryDescDiags(Board, CompleteListDiags) :-
-    getEveryDescDiagsHalfBoard(Board, 4, ListDiags1),
-    reverse(Board, ReversedBoard),
-    reverseEveryColumns(ReversedBoard, ReversedColumnsBoard),
-    getEveryDescDiagsHalfBoard(ReversedColumnsBoard, 4, ListDiags2),
-    reverse(ListDiags2, ReversedListDiags2),
-    reverseEveryColumns(ReversedListDiags2, ReversedColumnsListDiags2),
-    append(ListDiags1, ReversedColumnsListDiags2, CompleteListDiags).
-
-% Usage : Inverser toutes les colonnes du plateau.
-% reverseEveryColumns(+Board, -ReversedBoard)
-% - Board         : état du plateau après avoir joué le coup
-% - ReversedBoard : nouveau plateau avec des colonnes inversées par rapport à Board
-reverseEveryColumns([], []).
-reverseEveryColumns([Column|Rest], [ReversedColumn|ReversedRest]) :-
-    reverse(Column, ReversedColumn),
-    reverseEveryColumns(Rest, ReversedRest).
-
-% Usage : Créer une liste de diagonales de la moitié du plateau Board.
-%         Les numéros de ces diagonales sont compris entre Rank et 6.
-%         Rank est un entier entre 1 et 6 inclus.
-% getEveryDescDiagsHalfBoard(+Board, +Rank, -ListDiags)
-% - Board     : état du plateau après avoir joué le coup
-% - Rank      : rang à partir duquel on veut les diagonales
-% - ListDiags : listes des diagonales de numéro Rank au numéro 6
-getEveryDescDiagsHalfBoard(_, 7, []).
-getEveryDescDiagsHalfBoard(Board, Rank, [Diag|Rest]) :-
-    createOneDescDiag(Board, Rank, 1, Rank, Diag),
-    NextRank is Rank + 1,
-    getEveryDescDiagsHalfBoard(Board, NextRank, Rest).
-
-% Usage : Créer une liste représentant une diagonale de numéro Rank
-% createOneDescDiag(+Board, +Rank, 1, +Rank, -Diag)
-% - Board       : état du plateau après avoir joué le coup
-% - Rank        : rang de la diagonale créée (Entier entre 1 et 6 inclus)
-% - IndexColumn : index de la colonne du jeton à mettre dans la diagonale (initialisé à 1 au premier appel)
-% - IndexToken  : index du jeton dans sa colonne (initialisé à Rank au premier appel)
-% - Diag        : diagonale créée
-createOneDescDiag(_, Rank, IndexColumn, 0, []) :-
-	IndexColumn is Rank + 1,!.
-createOneDescDiag(Board, Rank, IndexColumn, IndexToken, [Token|Rest]) :-
-    nth1(IndexColumn, Board, Column),
-    nth1(IndexToken, Column, Token),
-    NewIndexColumn is IndexColumn+1,
-    NewIndexToken is IndexToken-1,
-    createOneDescDiag(Board, Rank, NewIndexColumn, NewIndexToken, Rest).
 
 
 %%% TESTS %%%
